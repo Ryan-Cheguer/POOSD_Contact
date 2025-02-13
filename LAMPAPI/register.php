465a82bb83baca5710129a1d396665bfbaf44748
Elephoont <haydenhuening@icloud.com>
2025-02-13 00:54:51 -0500

    Semicolons
    Added some mission semicolons

--- LAMPAPI/register.php
+++ LAMPAPI/register.php
@@ -1,12 +1,12 @@
 <?php
-    require 'db.php'
+    require 'db.php';
     $inData = getRequestInfo();
     if(!$inData){
         echo "Error: Invalid JSON data received.";
     }
 
     $stmt = $conn->prepare("INSERT INTO users (FirstName, LastName, Username, Pass) VALUES (?, ?, ?, ?)");
-    $stmt->bind_param("ssss", $inData["firstName"], $inData["lastName"], $inData["username"], $inData["password"])\
+    $stmt->bind_param("ssss", $inData["firstName"], $inData["lastName"], $inData["username"], $inData["password"]);
     $stmt->execute();
 
     if($stmt->affected_rows > 0){
