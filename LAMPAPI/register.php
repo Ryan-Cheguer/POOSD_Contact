36b0422a7a77c5855ee610e8135f0ab5d4a827af
Elephoont <haydenhuening@icloud.com>
2025-02-13 00:09:51 -0500

    Fixed variables in register.php
    Fixed the variables in register.php so it references them from $inData[]

--- LAMPAPI/register.php
+++ LAMPAPI/register.php
@@ -3,7 +3,7 @@
     $inData = getRequestInfo();
 
     $stmt = $conn->prepare("INSERT INTO users (FirstName, LastName, Username, Pass) VALUES (?, ?, ?, ?)");
-    $stmt->bind_param("ssss", $firstName, $lastName, $username, $password)\
+    $stmt->bind_param("ssss", $inData["firstName"], $inData["lastName"], $inData["username"], $inData["password"])\
     $stmt->execute();
 
     if($stmt->affected_rows > 0){
