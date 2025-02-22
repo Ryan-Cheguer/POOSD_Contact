1fc95652423c16fb4cf3f544e9c80a67b6bf6b1d
Elephoont <haydenhuening@icloud.com>
2025-02-22 02:29:15 -0500

    Duplicate Username Checks
    Updated register.php to check for valid input as well as duplicate usernames.

--- LAMPAPI/register.php
+++ LAMPAPI/register.php
@@ -5,6 +5,23 @@
         echo "Error: Invalid JSON data received.";
     }
 
+    // Check if input is valid
+    if($inData["firstName"] == '' || $inData["lastName"] == '' || $inData["username"] == '' || $inData["password"] == ''){
+        echo "Error: Invalid input.";
+        exit();
+    }
+
+    // Check if username is already taken
+    $stmt = $conn->prepare("SELECT * FROM users WHERE Username = ?");
+    $stmt->bind_param("s", $inData["username"]);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if($result->fetch_assoc()){
+        echo "Error: Username already taken.";
+        exit();
+    }
+
+    // Insert into database
     $stmt = $conn->prepare("INSERT INTO users (FirstName, LastName, Username, Pass) VALUES (?, ?, ?, ?)");
     $stmt->bind_param("ssss", $inData["firstName"], $inData["lastName"], $inData["username"], $inData["password"]);
     $stmt->execute();
