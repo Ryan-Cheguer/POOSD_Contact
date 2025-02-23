65b388f8ef0f8661d08b1e0ed48c228ef03b41e0
Elephoont <haydenhuening@icloud.com>
2025-02-22 19:02:43 -0500

    Fixed retrieve and search contacts
    Retrieve and search contacts now both work. Both also now include pageSize as an option. 'page' and 'pageSize' are optional and default to 1 and 10 respectively if not included in the JSON from the client.

--- LAMPAPI/retrieve_contacts.php
+++ LAMPAPI/retrieve_contacts.php
@@ -1,25 +1,41 @@
 <?php
     require 'db.php';
     require 'auth.php';
-    use Firebase\JWT\JWT;
 
-    $user = verifyToken();
+    // Default values
+    $contactsPerPage = 10;
+    $pageNum = 1;
 
     // Get JSON from client
     $inData = getRequestInfo();
-    if(!$inData){
-        echo 'Error: Invalid JSON data received'
+
+    $user = verifyToken();
+
+    // Make sure input is valid. Use default values if optional values not provided
+    if(isset($inData['page']) && is_numeric($inData['page'])){
+        if($inData['page'] < 1){
+            echo 'Error: Invalid page number';
+            exit();
+        }
+        $pageNum = $inData['page'];
+        $pageNum = intval($pageNum);
     }
 
-    // Make sure input is valid
-    if($inData['page'] < 1){
-        echo 'Error: Invalid page number';
-        exit();
+    if(isset($inData['pageSize']) && is_numeric($inData['pageSize'])){
+        if($inData['pageSize'] < 1){
+            echo 'Error: Invalid page size';
+            exit();
+        }
+        $contactsPerPage = $inData['pageSize'];
+        $contactsPerPage = intval($contactsPerPage);
     }
+
+    // Calculate offset
+    $offset = ($pageNum - 1) * $contactsPerPage;
 
     // Get contacts from database
-    $stmt = $conn->prepare("SELECT * FROM contacts WHERE UserID = ? LIMIT 10 OFFSET ?");
-    $stmt->bind_param("ii", $user['id'], ($inData['page'] - 1) * 10);
+    $stmt = $conn->prepare("SELECT * FROM contacts WHERE UserID = ? LIMIT $contactsPerPage OFFSET $offset");
+    $stmt->bind_param("i", $user['id']);
     $stmt->execute();
     $result = $stmt->get_result();
 
--- LAMPAPI/search_contacts.php
+++ LAMPAPI/search_contacts.php
@@ -1,29 +1,53 @@
 <?php
     require 'db.php';
     require 'auth.php';
-    use Firebase\JWT\JWT;
 
-    $user = verifyToken();
+    // Default values
+    $contactsPerPage = 10;
+    $pageNum = 1;
 
     // Get JSON from client
     $inData = getRequestInfo();
     if(!$inData){
-        echo 'Error: Invalid JSON data received'
-    }
-
-    // Make sure input is valid
-    if($inData['search'] == '' ){
-        echo 'Error: Invalid search term';
-        exit();
-    } else if($inData['page'] < 1){
-        echo 'Error: Invalid page number';
+        echo 'Error: Invalid JSON data received';
         exit();
     }
 
-    // Get contacts from database
-    $stmt = $conn->prepare("SELECT * FROM contacts WHERE UserID = ? AND (FirstName LIKE ? OR LastName LIKE ? OR Phone LIKE ? OR Email LIKE ?) LIMIT 10 OFFSET ?");
+    $user = verifyToken();
+
+    // Make sure input is valid. Use default values if optional values not provided
+    if(isset($inData['page']) && is_numeric($inData['page'])){
+        if($inData['page'] < 1){
+            echo 'Error: Invalid page number';
+            exit();
+        }
+        $pageNum = $inData['page'];
+        $pageNum = intval($pageNum);
+    }
+
+    if(isset($inData['pageSize']) && is_numeric($inData['pageSize'])){
+        if($inData['pageSize'] < 1){
+            echo 'Error: Invalid page size';
+            exit();
+        }
+        $contactsPerPage = $inData['pageSize'];
+        $contactsPerPage = intval($contactsPerPage);
+    }
+
+    if(!isset($inData['search']) || $inData['search'] == '' ){
+        echo 'Error: A search term is required';
+        exit();
+    }
+
+    // Calculate offset
+    $offset = ($pageNum - 1) * $contactsPerPage;
+
+    // Prepare search term
     $search = '%' . $inData['search'] . '%';
-    $stmt->bind_param("issss", $user['id'], $search, $search, $search, $search, ($inData['page'] - 1) * 10);
+    
+    // Get contacts from database
+    $stmt = $conn->prepare("SELECT * FROM contacts WHERE UserID = ? AND (FirstName LIKE ? OR LastName LIKE ? OR Phone LIKE ? OR Email LIKE ?) LIMIT $contactsPerPage OFFSET $offset");
+    $stmt->bind_param("issss", $user['id'], $search, $search, $search, $search);
     $stmt->execute();
     $result = $stmt->get_result();
 
